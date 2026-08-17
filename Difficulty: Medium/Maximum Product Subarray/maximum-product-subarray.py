232b72fd7fac867f9b45d696fdc0139b2e3acee7
yogesh4617 <yogeshyogesh10514@gmail.com>
2026-08-17 15:39:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.py
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.py
@@ -1,25 +1,27 @@
 class Solution:
 	def maxProduct(self,arr):
 		# code here
-		
-		max_product=float('-inf')
-		
-		left_to_right=1
-		right_to_left=1
-		
-		n=len(arr)
+		max_Produt=-9999999
+		L_to_R = 1
+		R_to_L = 1
+		n = len(arr)
 		
 		for i in range(n):
+		    # 1 calculate the prefix product
+		    L_to_R*=arr[i]
 		    
-            left_to_right *= arr[i]
-            
-            right_to_left *= arr[n - 1 - i]
-            
-            max_product = max(max_product, left_to_right, right_to_left)
-            
-            if left_to_right == 0:
-                left_to_right = 1
-            if right_to_left == 0:
-                right_to_left = 1
-		 
-        return max_product         
+		    # 2 calculate the suffix product 
+		    R_to_L*=arr[n-1-i]
+		    
+		    # Find the max value
+		    max_Produt = max(max_Produt, L_to_R, R_to_L)
+		    
+		    # 4 if the value hits 0, reset the value into 1
+		    if L_to_R == 0:
+		        L_to_R = 1
+		        
+		    if R_to_L == 0:
+		        R_to_L = 1
+		        
+		        
+        return max_Produt
